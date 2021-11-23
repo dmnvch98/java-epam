dc2b00347f2587a585842206eaca112af8bc9adf
dmn98 <wildehaifisch@gmail.com>
2021-11-23 22:58:30 +0300

    small refactoring

--- src/learn/tasks/task1/ArgumentsViceVersa.java
+++ src/learn/tasks/task1/ArgumentsViceVersa.java
@@ -2,16 +2,12 @@ package learn.tasks.task1;
 
 public class ArgumentsViceVersa {
     public static void main(String[] args) {
-        new ArgumentsViceVersa().printArguments();
-    }
-    public static String askArguments(){
-        return Utils.ask("enter nums separated by space:");
+        new ArgumentsViceVersa().printArguments(args);
     }
 
-    public void printArguments(){
-        char[] argumentsArr = askArguments().toCharArray();
-        for (int i = argumentsArr.length - 1; i >= 0; i--){
-            System.out.print(argumentsArr[i]);
+    public void printArguments(String[] arr){
+        for (int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i] + " ");
         }
     }
 }
